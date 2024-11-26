df3a7de8fbd61eade5d8e6b0c93e5d249e376dad
deluxesande <deluxesande@gmail.com>
2024-11-26 17:45:56 +0300

    halfway got the bottom navigation to show

--- java/me/deluxesande/dopify/fragments/Homepage.java
+++ java/me/deluxesande/dopify/fragments/Homepage.java
@@ -1,19 +1,22 @@
-package me.deluxesande.dopify;
+package me.deluxesande.dopify.fragments;
 
 import android.os.Bundle;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import me.deluxesande.dopify.R;
+
+public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_homepage);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

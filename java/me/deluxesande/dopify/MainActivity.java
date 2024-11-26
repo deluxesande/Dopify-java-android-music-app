a5bf60eab05133818c3ca434abe19c3c415c4ba9
deluxesande <deluxesande@gmail.com>
2024-11-26 18:16:28 +0300

    Fully working navigation in java android

--- java/me/deluxesande/dopify/MainActivity.java
+++ java/me/deluxesande/dopify/MainActivity.java
@@ -1,23 +1,48 @@
 package me.deluxesande.dopify;
 
 import android.os.Bundle;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+import me.deluxesande.dopify.fragments.HomePage;
+import me.deluxesande.dopify.fragments.ProfilePage;
+import me.deluxesande.dopify.fragments.SearchPage;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.home);
+        bottomNavigationView.setOnItemSelectedListener(navListener);
+
+        Fragment selectedFragment = new HomePage();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
     }
+
+    private NavigationBarView.OnItemSelectedListener navListener = item -> {
+        int itemId = item.getItemId();
+
+        Fragment selectedFragment = null;
+
+        if (itemId == R.id.home) {
+            selectedFragment = new HomePage();
+        } else if (itemId == R.id.search) {
+            selectedFragment = new SearchPage();
+        } else if (itemId == R.id.profile) {
+            selectedFragment = new ProfilePage();
+        }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+        return true;
+    };
 }
---
+++ java/me/deluxesande/dopify/fragments/SearchPage.java
@@ -0,0 +1,25 @@
+package me.deluxesande.dopify.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import me.deluxesande.dopify.R;
+
+public class SearchPage extends Fragment {
+
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        view = inflater.inflate(R.layout.fragment_searchpage, container, false);
+        return view;
+    }
+}

92e9a4f91939d4714d87d23d500cb40afe754efe
deluxesande <deluxesande@gmail.com>
2024-12-03 10:52:01 +0300

    Simple API calls with Volley

--- java/me/deluxesande/dopify/MainActivity.java
+++ java/me/deluxesande/dopify/MainActivity.java
@@ -1,8 +1,11 @@
 package me.deluxesande.dopify;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
--- java/me/deluxesande/dopify/fragments/HomePage.java
+++ java/me/deluxesande/dopify/fragments/HomePage.java
@@ -1,28 +1,36 @@
 package me.deluxesande.dopify.fragments;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import me.deluxesande.dopify.ApiClient;
+import me.deluxesande.dopify.MainActivity;
 import me.deluxesande.dopify.R;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 
-import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class HomePage extends Fragment {
 
     View view;
+    Button end_button;
 
     @Nullable
     @Override
@@ -43,6 +51,50 @@ public class HomePage extends Fragment {
         Glide.with(this).load(imageUrl).into(musicCover2);
         Glide.with(this).load(imageUrl).into(recommended_1);
         Glide.with(this).load(imageUrl).into(best_1);
+
+        // Test API logic
+        end_button = view.findViewById(R.id.end_button);
+
+        end_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Instantiate the RequestQueue.
+                RequestQueue queue = Volley.newRequestQueue(getContext());
+                String url = "https://api.restful-api.dev/objects";
+
+                JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(getContext(), response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getContext(), "That didn't work!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                // Add the request to the RequestQueue.
+                queue.add(request);
+
+                // Request a string response from the provided URL.
+//                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+//                        new Response.Listener<String>() {
+//                            @Override
+//                            public void onResponse(String response) {
+//                                // Display the first 500 characters of the response string.
+//                                Toast.makeText(getContext(), response, Toast.LENGTH_SHORT).show();
+//                            }
+//                        }, new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Toast.makeText(getContext(), "That didn't work!", Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+
+            }
+        });
 
         return view;
     }
